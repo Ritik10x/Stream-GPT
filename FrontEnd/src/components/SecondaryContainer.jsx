d40e1ca9b6e4795f6d0940c94beb2a01a84da7a5
RITIK SINGH <111556793+Ritik10x@users.noreply.github.com>
2026-01-18 18:19:03 +0530

    FrontEnd Updated

--- FrontEnd/src/components/SecondaryContainer.jsx
+++ FrontEnd/src/components/SecondaryContainer.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
+  const movies = useSelector(store=>store.movies)
   return (
-    <div>
-
+  movies.nowPlayingMovies && (
+    <div className='-mt-60 relative z-25 '>
+      
+<MovieList title={'Now Playing'} movies={movies.nowPlayingMovies}/>
+<MovieList title={'Trending'} movies={movies.nowPlayingMovies}/>
+<MovieList title={'Popular'} movies={movies.nowPlayingMovies}/>
+<MovieList title={'Upcoming'} movies={movies.nowPlayingMovies}/>
+<MovieList title={'Horror'} movies={movies.nowPlayingMovies}/>
     </div>
   )
+)
 }
 
 export default SecondaryContainer
--- FrontEnd/src/components/VideoTitle.jsx
+++ FrontEnd/src/components/VideoTitle.jsx
@@ -6,7 +6,7 @@ const VideoTitle = ({ title, overview }) => {
 
 
   return (
-    <div className=' w-screen aspect-video  pt-36 px-36 absolute text-white bg-linear-to-r from-black'>
+    <div className=' w-screen aspect-video  pt-36 px-36 absolute text-white bg-linear-to-r from-black z-20'>
       <h1 className='text-6xl font-bold '>{title}</h1>
       <p className='py-6 text-lg w-1/4'>{overview}</p>
       <div className='flex'>
