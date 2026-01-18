430b33e459bd7017bf73707b438bd64e3ec19706
RITIK SINGH <111556793+Ritik10x@users.noreply.github.com>
2026-01-18 17:13:03 +0530

    Front End Updates

--- FrontEnd/src/components/VideoTitle.jsx
+++ FrontEnd/src/components/VideoTitle.jsx
@@ -6,12 +6,12 @@ const VideoTitle = ({ title, overview }) => {
 
 
   return (
-    <div className=' w-screen aspect-video  pt-[10%] px-24 absolute text-white bg-linear-to-r from-black'>
+    <div className=' w-screen aspect-video  pt-36 px-36 absolute text-white bg-linear-to-r from-black'>
       <h1 className='text-6xl font-bold '>{title}</h1>
       <p className='py-6 text-lg w-1/4'>{overview}</p>
       <div className='flex'>
-        <button className='flex items-center gap-3 bg-gray-500/50  text-white text-xl py-4 px-12 rounded-lg '><FaPlay />Play</button>
-        <button className='flex items-center gap-3 mx-2 bg-gray-500/50  text-white text-xl py-4 px-16 rounded-lg '><CiCircleInfo size={32} />More Info</button>
+        <button className='flex items-center gap-3 bg-white  text-black text-xl py-4 px-12 rounded-lg hover:bg-white/80'><FaPlay />Play</button>
+        <button className='flex items-center gap-3 mx-2 bg-gray-500/50  text-white text-xl py-4 px-12 rounded-lg '><CiCircleInfo size={32} />More Info</button>
       </div>
     </div>
   )
